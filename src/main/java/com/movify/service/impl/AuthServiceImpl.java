fa51536933b6d6e5cb17bd7d6dc7c4e6020a17d1
Adeleke Oladapo <adelekeoladapo@gmail.com>
2020-01-04 19:41:48 +0100

    Login completed

--- src/main/java/com/movify/service/impl/AuthServiceImpl.java
+++ src/main/java/com/movify/service/impl/AuthServiceImpl.java
@@ -3,22 +3,35 @@ package com.movify.service.impl;
 import com.movify.dto.*;
 import com.movify.enums.EmailMessages;
 import com.movify.enums.Message;
+import com.movify.enums.Status;
 import com.movify.model.User;
 import com.movify.model.repository.UserRepository;
 import com.movify.security.Hash;
 import com.movify.service.AuthService;
+import com.movify.service.CacheService;
 import com.movify.service.EmailService;
+import com.typesafe.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.time.LocalDateTime;
 
 public class AuthServiceImpl implements AuthService {
 
+    final Logger LOGGER = LoggerFactory.getLogger(AuthServiceImpl.class);
+
+    @Inject
+    Config config;
+
     @Inject
     UserRepository userRepository;
 
     @Inject
     EmailService emailService;
+
+    @Inject
+    CacheService cacheService;
 
     @Override
     public ServiceResponse register(UserCreationRequest request) {
@@ -40,7 +53,44 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public ServiceResponse login(LoginRequest request) {
-        return null;
+        ServiceResponse res = new ServiceResponse();
+        res.setCode(Message.ERROR);
+        res.setMessage(Message.GENERAL_ERROR_MESSAGE);
+        User user = userRepository.findByEmail(request.getEmail());
+        if(user == null){
+            LOGGER.error(String.format("Login failed. User %s does not exist", request.getEmail()));
+            res.setMessage(Message.USER_DOES_NOT_EXIST_MESSAGE);
+            return res;
+        }
+
+        if(user.getFailedLoginCount() > this.config.getInt("system.login.count.max")){
+            user.setStatus(Status.BLOCKED);
+            res.setMessage(Message.USER_ACCOUNT_BLOCKED_MESSAGE);
+            return res;
+        }
+
+        if(!Hash.checkPassword(request.getPassword(), user.getPasswordHash())){
+            int failedLoginCount = user.getFailedLoginCount() + 1;
+            user.setFailedLoginCount(failedLoginCount);
+            userRepository.save(user);
+            res.setMessage(Message.INVALID_PASSWORD);
+            return res;
+        }
+
+        user.setFailedLoginCount(0);
+        user.setLastLoggedIn(LocalDateTime.now());
+
+        LoginServiceResponse loginResponse = new LoginServiceResponse();
+        String token = "toyotaavalon2009"; // TODO Please free token when we're ready SecurityConstants.TOKEN_PREFIX.concat(UUID.randomUUID().toString());
+        loginResponse.setId(user.getId());
+        loginResponse.setToken(token);
+        loginResponse.setFullName(user.getFullName());
+        loginResponse.setEmail(user.getEmail());
+        userRepository.save(user);
+        this.cacheService.storeUser(user, token);
+        res.setData(loginResponse);
+
+        return res.setCode(Message.SUCCESS).setMessage(Message.GENERAL_SUCCESS_MESSAGE);
     }
 
     @Override
